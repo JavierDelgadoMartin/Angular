b50acdb895c9777284d434c8e9bef4e2843acd6b
javi delgado <javi delgado>
2017-02-02 20:26:43 +0100

    commit

--- APIMarvel/src/app/app.module.ts
+++ APIMarvel/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PersonajesComponent } from './personajes/personajes.component';
+import { SeriesComponent } from './series/series.component';
+import { ComicsComponent } from './comics/comics.component';
+import { PieComponent } from './pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PersonajesComponent,
+    SeriesComponent,
+    ComicsComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule,
